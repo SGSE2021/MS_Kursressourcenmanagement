ca612cc3f611478427201589a8753b015173c43a
Dominik Löwen <dominik.loewen1@fh-bielefeld.de>
2021-06-28 15:20:34 +0200

    Creates Server with access to MongoDB-Appointments

---
+++ server/routes/appointment-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const AppointmentsCtrl = require('../controllers/appointment-ctrl')
+
+const router = express.Router()
+
+router.post('/appointment', AppointmentsCtrl.createAppointment)
+router.put('/appointment/:id', AppointmentsCtrl.updateAppointment)
+router.delete('/appointment/:id', AppointmentsCtrl.deleteAppointment)
+router.get('/appointment/:id', AppointmentsCtrl.getAppointmentById)
+router.get('/appointments', AppointmentsCtrl.getAppointments)
+
+module.exports = router
