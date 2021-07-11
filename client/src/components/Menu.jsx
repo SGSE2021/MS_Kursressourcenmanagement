82c6134a55bac750d71871ffee32776c00a66da9
Dominik Löwen <dominik.loewen1@fh-bielefeld.de>
2021-07-11 12:35:46 +0200

    changes paths in menu router

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -179,16 +179,15 @@ function Menu(props) {
         <div className={classes.drawerHeader} />
         <Router>
           <Switch>
-            <Route path="/resources/" exact component={CourseOverview}/>
-            <Route path={`/resources/course/:id/appointments`} exact component={AppointmentOverview} />
-            <Route path={`/resources/course/:id/appointments/create`} exact component={CreateAppointment} />
-            <Route path={`/resources/course/:id/appointments/update/:apId`} exact component={UpdateAppointment} />
-            <Route path={`/resources/course/:id/members`} exact component={MembersOverview} />
-            <Route path={`/resources/course/:id/`} exact component={ContentOverview} />
-            <Route path={`/resources/course/:id/upload`} exact component={FileUpload} />
+            <Route path="https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/" exact component={CourseOverview}/>
+            <Route path={`/https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/course/:id/appointments`} exact component={AppointmentOverview} />
+            <Route path={`/https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/course/:id/appointments/create`} exact component={CreateAppointment} />
+            <Route path={`/https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/course/:id/appointments/update/:apId`} exact component={UpdateAppointment} />
+            <Route path={`/https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/course/:id/members`} exact component={MembersOverview} />
+            <Route path={`/https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/course/:id/`} exact component={ContentOverview} />
+            <Route path={`/https://sgse2021-ilias.westeurope.cloudapp.azure.com/resources/course/:id/upload`} exact component={FileUpload} />
           </Switch>
         </Router>
-
       </main>
       </div>
     )
